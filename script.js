90e0146e474aaa7e9e7bda6ff0128cb5a25cb3c0
Elisabeth Strunk <elisabeth.maria.strunk@gmail.com>
2020-05-01 23:56:30 +0100

    feature: update the clock every second

--- script.js
+++ script.js
@@ -8,15 +8,20 @@ const SECONDHAND = document.querySelector("#second");
 const MINUTEHAND = document.querySelector("#minute");
 const HOURHAND = document.querySelector("#hour");
 
-// Determine current time
-var now = new Date();
+function updateClockHands() {
+    // Determine current time
+    var now = new Date();
 
-// Calculate the degrees of rotation for all clock hands
-var secondPosition = now.getSeconds() * 6
-var minutePosition = now.getMinutes() * 6 + 0.1 * now.getSeconds()
-var hourPosition = now.getHours() * 30 + 0.5 * now.getMinutes()
+    // Calculate the degrees of rotation for all clock hands
+    var secondPosition = now.getSeconds() * 6
+    var minutePosition = now.getMinutes() * 6 + 0.1 * now.getSeconds()
+    var hourPosition = now.getHours() * 30 + 0.5 * now.getMinutes()
 
-// Rotate the clock hand elements to bring the clock hands in the right positions
-SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)"
-MINUTEHAND.style.transform = "rotate(" + minutePosition + "deg)"
-HOURHAND.style.transform = "rotate(" + hourPosition + "deg)"
+    // Rotate the clock hand elements to bring the clock hands in the right positions
+    SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)"
+    MINUTEHAND.style.transform = "rotate(" + minutePosition + "deg)"
+    HOURHAND.style.transform = "rotate(" + hourPosition + "deg)"
+}
+
+updateClockHands()
+setInterval(updateClockHands, 1000)
