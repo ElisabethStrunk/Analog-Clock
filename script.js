da5d9dd4f0f225a1a5ef05816353f57886ae1c50
Elisabeth Strunk <elisabeth.maria.strunk@gmail.com>
2020-05-01 23:59:04 +0100

    fix: animation now works properly for full circle; before, the handle ran 360deg backwards every time it reached the 0deg position

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const SECONDHAND = document.querySelector("#second");
 const MINUTEHAND = document.querySelector("#minute");
 const HOURHAND = document.querySelector("#hour");
 
+var secondNumberOfTurns = 0
+var minuteNumberOfTurns = 0
+var hourNumberOfTurns = 0
+
 function updateClockHands() {
     // Determine current time
     var now = new Date();
@@ -16,6 +20,14 @@ function updateClockHands() {
     var secondPosition = now.getSeconds() * 6
     var minutePosition = now.getMinutes() * 6 + 0.1 * now.getSeconds()
     var hourPosition = now.getHours() * 30 + 0.5 * now.getMinutes()
+
+    if (secondPosition == 0) { secondNumberOfTurns++ }
+    if (minutePosition == 0) { minuteNumberOfTurns++ }
+    if (hourPosition == 0) { hourNumberOfTurns++ }
+
+    secondPosition = now.getSeconds() * 6 + secondNumberOfTurns * 360
+    minutePosition += minuteNumberOfTurns * 360
+    hourPosition += hourNumberOfTurns * 360
 
     // Rotate the clock hand elements to bring the clock hands in the right positions
     SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)"
