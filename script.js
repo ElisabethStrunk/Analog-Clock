5840c81ab2558aa26f17b9d975fd24145382861c
Elisabeth Strunk <elisabeth.maria.strunk@gmail.com>
2020-05-01 23:14:11 +0100

    feature: display the correct clock hand positions for the current time

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+    Code adapted from course material by Morten Rand-Hendriksen found at
+    https://www.linkedin.com/learning/javascript-essential-training-3
+*/
+
+// Select all elements representing the clock hands
+const SECONDHAND = document.querySelector("#second");
+const MINUTEHAND = document.querySelector("#minute");
+const HOURHAND = document.querySelector("#hour");
+
+// Determine current time
+var now = new Date();
+
+// Calculate the degrees of rotation for all clock hands
+var secondPosition = now.getSeconds() * 6
+var minutePosition = now.getMinutes() * 6 + 0.1 * now.getSeconds()
+var hourPosition = now.getHours() * 30 + 0.5 * now.getMinutes()
+
+// Rotate the clock hand elements to bring the clock hands in the right positions
+SECONDHAND.style.transform = "rotate(" + secondPosition + "deg)"
+MINUTEHAND.style.transform = "rotate(" + minutePosition + "deg)"
+HOURHAND.style.transform = "rotate(" + hourPosition + "deg)"
